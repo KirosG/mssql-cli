e98e2b41c5b9f8827a75cc1aab0e28993a76deae
Elliot Boschwitz <elliotboschwitz@Elliots-MBP.guest.corp.microsoft.com>
2020-01-03 14:08:38 -0800

    edited python install

--- build_scripts/debian/build.sh
+++ build_scripts/debian/build.sh
@@ -22,7 +22,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 debian_directory_creator=$script_dir/dir_creator.sh
 
 # Install dependencies for the build
-sudo apt-get install -y libssl-dev libffi-dev debhelper
+sudo apt-get install -y libssl-dev libffi-dev debhelper python-dev
 # Download, Extract, Patch, Build CLI
 tmp_pkg_dir=$(mktemp -d)
 working_dir=$(mktemp -d)
@@ -38,11 +38,11 @@ rm -rf $source_dir/../debian_output
 [ -d $local_repo/privates ] && cp $local_repo/privates/*.whl $tmp_pkg_dir
 
 # Build Python from source and include
-python_dir=$(mktemp -d)
-python_archive=$(mktemp)
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz -qO $python_archive
-tar -xvzf $python_archive -C $python_dir
-echo "Python dir is $python_dir"
+# python_dir=$(mktemp -d)
+# python_archive=$(mktemp)
+# wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz -qO $python_archive
+# tar -xvzf $python_archive -C $python_dir
+# echo "Python dir is $python_dir"
 
 #  clean any previous make files
 make clean || echo "Nothing to clean"
